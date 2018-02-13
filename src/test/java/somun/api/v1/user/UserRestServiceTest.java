9047c04b6c3535ab72ff494efda4f7edd32319a7
장재훈 <codelife@11st.co.kr>
2018-02-13 17:08:13 +0900

    package rename & etc

--- src/test/java/somun/api/v1/user/UserRestServiceTest.java
+++ src/test/java/somun/api/v1/user/UserRestServiceTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 import somun.core.util.LogUtil;
@@ -21,7 +21,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 @RunWith(SpringRunner.class)
-@ActiveProfiles(profiles = "local")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Transactional
 public class UserRestServiceTest {
@@ -59,11 +58,33 @@ public class UserRestServiceTest {
 
         user = restTemplate.postForObject("/User/V1/AddUser", user, User.class);
 
+
         new LogUtil().printObject(user);
 
         assertThat(user).isNotNull();
 
     }
+
+
+    @Test
+    @Commit
+    public void UpdateUser(){
+
+        Date date = new Date();
+        User user = User.builder()
+                .userStat("S2")
+                .build();
+
+        log.debug("############");
+
+        String post = restTemplate.patchForObject("/User/V1/UpdateUser/1", user, String.class);
+
+        log.debug("############");
+        log.debug(post);
+        assertThat(post).isNotEmpty();
+
+    }
+
 
 
 }
